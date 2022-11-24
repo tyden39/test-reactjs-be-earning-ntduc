cad2c582547019402259e16947fea20c5833fb97
ducnguyen <ntanduc94@gmail.com>
2022-11-24 20:03:09 +0700

    create Test 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { getEnglishList } from './Test2';
+import Test3 from './Test3';
 
 function App() {
+  const {list6, list18, list24} = getEnglishList()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App-body'>
+        <Test3 />
+      </div>
     </div>
   );
 }
---
+++ src/Test3/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.scss'
+
+const Test3 = () => {
+  return (
+    <div>Test3</div>
+  )
+}
+
+export default Test3
