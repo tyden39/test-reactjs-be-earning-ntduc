f60353a7f9e05980709ccb58bffc4b9b8b11f8a0
ducnguyen <ntanduc94@gmail.com>
2022-11-27 23:50:43 +0700

    update

--- src/Test3/components/List6/index.jsx
+++ src/Test3/components/List6/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
 const List6 = ({data}) => {
+
+
   return (
     <div className='word-list2'>
       {data.map((item, index) => 
         <div key={`list6-${index}`} className="word-list2__item2">
           <div className='word-list2__item2__index'><span>{item?.primary}</span></div>
-          {item?.list?.map((word, index) => <span key={`list6-item-${index}`} style={!index ? {marginLeft: '24px'} : {}} className='word-list2__item2__word'>{word}</span>)}
+          {item?.list?.map((word, index) => 
+            <span 
+              key={`list6-item-${index}`} 
+              style={!index ? {marginLeft: '24px'} : {}} 
+              className={`word-list2__item2__word`}>{word}
+            </span>
+          )}
         </div>
       )}
     </div>
